570d73ce7ffc49baca9818a355c259ee5f61aebe
Christopher Castro <christopherdarielc@gmail.com>
2024-03-26 00:28:53 -0600

    Agregando y terminando proyecto de contador de clics

---
+++ free-code-camp-yt2-clic-counter/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { useState, useStateb } from 'react';
+import freeCodeCampLogo from './img/banner 2.jpg'
+import Boton from './components/Button.js';
+import Contador from './components/Contador.js';
+function App() {
+
+  const [numClics, setNumClics]= useState(0)
+
+  const manejarClic = () => {
+    setNumClics(numClics+1)
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0)
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor' >
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='logo de frecodecamp'
+        />
+      </div>
+
+      <div className='contenedor-principal' >
+
+        <Contador
+        numClics={numClics}/>
+
+        <Boton
+          texto='Clic'
+          esBotonDeclic={true}
+          manejarClic={manejarClic} />
+
+        <Boton
+          texto='reiniciar'
+          esBotonDeclic={false}
+          manejarClic={reiniciarContador} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
